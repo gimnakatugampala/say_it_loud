6d9a5a774f79b7c0359104d7e58d4c4b05f04850
gimnakatugampala <gimnakatugampala1@gmail.com>
2021-09-22 12:55:28 +0530

    helpers delete

--- helpers/hbs.js
+++ helpers/hbs.js
@@ -7,11 +7,16 @@ module.exports = {
     firstLower:function(name){
         return name.toLowerCase()
     },
-    displayBin:function(uid,postuid){
+    displayBin:function(uid,postuid,post){
         
         if(uid == postuid){
            return `
-           <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="far fa-trash-alt"></i></a>
+           <form action="/posts/${post}" method="post">
+              <input type="hidden" name="_method" value="DELETE">
+              <button type="submit" class="btn-floating halfway-fab waves-effect waves-light red">
+                <i class="far fa-trash-alt"></i>
+              </button>
+          </form>
            `
         }
     }
